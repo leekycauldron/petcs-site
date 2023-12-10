e1547d1289949e880ee32564e3627aa929e85400
Bryson <48774118+leekycauldron@users.noreply.github.com>
2023-12-10 12:23:32 -0500

    police login

---
+++ escape/police.js
@@ -0,0 +1,22 @@
+
+document.getElementById('login-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('https://petcs-police-login-tight-rain-c3a6.brysonleekwen.workers.dev/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        });
+
+        const message = await response.text();
+        console.log(message)
+    } catch (error) {
+        console.error('An error occurred:', error);
+        console.log('Error sending request.');
+    }
+});
+
